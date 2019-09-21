54114e991ca6e06e897228a250807b9b65d786c3
buheshagoulilun <mxw210@nyu.edu>
2019-09-21 21:11:07 +0800

    i

---
+++ src/App.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+
+function App() {
+  return (
+
+    <div className="App">
+      <link href ="C:\Users\24245\666\src\App.css"rel="stylesheet" type="text/css"/>
+      <title>assignment</title>
+      <div class="header">
+      <h1><header>negative impacts of digital life </header></h1>
+      <p>peter</p>
+      </div>
+    
+      <div class="side">
+        <h2>User</h2>
+        <a href="https://imggmi.com" target="_blank"><img src="https://cdn1.imggmi.com/uploads/2019/9/13/276272b44da5997245532cb42d75f735-full.png" border="0"/></a>
+        <h4>This is my friend Zoomhauer</h4>
+        <h2>Interface</h2>
+      <a href="https://imggmi.com" target="_blank"><img src="https://cdn1.imggmi.com/uploads/2019/9/13/4082e3ae124046753131e3a53b3758e7-full.jpg" border="0"/></a>
+      <h4>He is using Zhifubao</h4>
+      <h2>Address</h2>
+      <a href="https://imggmi.com" target="_blank"><img src="https://cdn1.imggmi.com/uploads/2019/9/13/614badf0169ef1224a799c2f7e5dc809-full.jpg" border="0"/></a>
+      <h4>Through the public WiFi</h4>
+      <h2>City</h2>
+      <a href="https://imggmi.com" target="_blank"><img src="https://cdn1.imggmi.com/uploads/2019/9/13/ccd75c4a9933306b115544765cccbee1-full.jpg" border="0"/></a>
+      <h4>His network operator is China Mobile</h4>
+      </div>
+      <div class="main">
+        <h2>Cloud</h2>
+        <a href="https://imggmi.com" target="_blank"><img src="https://cdn1.imggmi.com/uploads/2019/9/13/379c2b722cb6509a8de05a33d49f9242-full.jpg" border="0"/></a>
+      <h4>His account information is processed in Zhifubao</h4>
+      <h2>Earth</h2>
+      
+      <a href="https://imggmi.com" target="_blank"><img src="https://cdn1.imggmi.com/uploads/2019/9/13/5aaf879258a20719f3f8b02e64d63994-full.jpg" border="0"/></a>
+      <h4>A stranger stole his account information and his money</h4>
+
+      </div>
+
+
+
+
+    </div>
+  );
+}
+
+export default App;
